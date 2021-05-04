96a7a659671e10e661f9464c3388e838ee9112b9
tonitdiep <adiep123@gmail.com>
2021-05-04 12:54:32 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { GraceHopperQuoteComponent } from './GraceHopperQuoteComponent.js';
-import CatComponent from './CatComponent.js';
-import MouseComponent from './MouseComponent.js';
 
+import CatComponent from './CatComponent.js';
+import { GraceHopperQuoteComponent } from './GraceHopperQuoteComponent.js';
+import MouseComponent from './MouseComponent.js';
 
 class App extends Component {
 	render() {
